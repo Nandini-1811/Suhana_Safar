618d7424e4e9258d16859b9eef292e05c207c76e
Nandini-1811 <nandinigoel@gmail.com>
2026-03-05 19:52:23 +0530

    Initial commit - backend setup

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const dotenv = require("dotenv")
+const connectDB = require("./config/db")
+
+const userRoutes = require("./routes/userRoutes")
+
+
+dotenv.config()
+
+connectDB()
+
+
+const app = express()
+
+app.use(express.json())
+
+app.use("/api/users",userRoutes)
+
+
+app.get("/" , (req,res) => {
+    res.send("Welcome to Suhana Safar Transport API")
+});
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, ()=>{
+    console.log(`Server running on port ${PORT}`);
+})
