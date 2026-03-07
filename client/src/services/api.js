dbce0eb0f8efb55a504201c118854341359a44f2
Nandini-1811 <nandinigoel@gmail.com>
2026-03-07 23:20:27 +0530

    updated api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,13 +1,12 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://suhana-safar-backend.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
 
-  console.log("Sending token:", token);
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
