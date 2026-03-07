8f20f1577d672b40c7ccd32138460aa4f7988f81
Nandini-1811 <nandinigoel@gmail.com>
2026-03-08 00:25:13 +0530

    updated socket.io

--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -1,5 +1,7 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://suhana-safar-backend.onrender.com", {
+  transports: ["websocket", "polling"],
+});
 
 export default socket;
